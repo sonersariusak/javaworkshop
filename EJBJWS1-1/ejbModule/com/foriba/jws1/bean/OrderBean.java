14b034102aba366defc39a625c7906a0a871ae29
FIT\soner.sariusak <soner.sariusak@gmail.com>
2018-04-04 18:02:21 +0300

    -- kod formatlandı.

--- EJBJWS1-1/ejbModule/com/foriba/jws1/bean/OrderBean.java
+++ EJBJWS1-1/ejbModule/com/foriba/jws1/bean/OrderBean.java
@@ -14,11 +14,12 @@ import com.foriba.jws1.util.DateUtil;
 @SuppressWarnings("serial")
 @Stateless
 public class OrderBean extends ESGenericBean<BaseEntity> implements OrderService {
-	
-	 /**
-     * getByOrderedProductName metodun sorgusunda orderedProductName alanı için LIKE kullanılmıştır.
-     * @return records of JWS1_ORDER Tables
-     */
+
+	/**
+	 * getByOrderedProductName metodun sorgusunda orderedProductName alanı için LIKE kullanılmıştır.
+	 * 
+	 * @return records of JWS1_ORDER Tables
+	 */
 	@Override
 	public List<Jws1Order> getByOrderedProductName(String productName) throws Exception {
 		if(null != productName) {
@@ -29,10 +30,12 @@ public class OrderBean extends ESGenericBean<BaseEntity> implements OrderService
 		}
 
 	}
-	 /**
-     * getByOrderArrivalDate metodu, başlangıç ve bitiş orderArrivalDate arasındaki kayıtları getirir.
-     * @return records of JWS1_ORDER Tables
-     */
+
+	/**
+	 * getByOrderArrivalDate metodu, başlangıç ve bitiş orderArrivalDate arasındaki kayıtları getirir.
+	 * 
+	 * @return records of JWS1_ORDER Tables
+	 */
 	@Override
 	public List<Jws1Order> getByOrderArrivalDate(Timestamp startDate, Timestamp endDate) throws Exception {
 
@@ -40,10 +43,11 @@ public class OrderBean extends ESGenericBean<BaseEntity> implements OrderService
 
 	}
 
-	 /**
-     * mergeOrder metodu, eğer IDX'e göre eşleşen kayıt var ise güncellerler, yok ise yeni kayıt olarak ekler.
-     * @return records of JWS1_ORDER Tables
-     */
+	/**
+	 * mergeOrder metodu, eğer IDX'e göre eşleşen kayıt var ise güncellerler, yok ise yeni kayıt olarak ekler.
+	 * 
+	 * @return records of JWS1_ORDER Tables
+	 */
 	@Override
 	public String mergeOrder(Jws1Order jws) throws Exception {
 		merge(jws);
@@ -56,10 +60,11 @@ public class OrderBean extends ESGenericBean<BaseEntity> implements OrderService
 
 	}
 
-	 /**
-     * addOrder metodu, tabloya yeni kayıt ekler.
-     * @param 
-     */
+	/**
+	 * addOrder metodu, tabloya yeni kayıt ekler.
+	 * 
+	 * @param
+	 */
 	@Override
 	public String addOrder(String pName, Date orderDate, Timestamp orderArrivalDate, double amount, String clob, String blob) throws Exception {
 		String message = "";
@@ -93,10 +98,11 @@ public class OrderBean extends ESGenericBean<BaseEntity> implements OrderService
 		return message;
 	}
 
-	 /**
-     * updateOrder metodu, tabloyu günceller.
-     * @param 
-     */
+	/**
+	 * updateOrder metodu, tabloyu günceller.
+	 * 
+	 * @param
+	 */
 	@Override
 	public String updateOrder(long idx, String pName, double amount, String clob, String blob) throws Exception {
 		String message = "";
@@ -124,9 +130,10 @@ public class OrderBean extends ESGenericBean<BaseEntity> implements OrderService
 	}
 
 	/**
-     * updateAmountByOrderedProductName metodu, eşleşen orderedProductName'lerin Amount'larını günceller.
-     * @param 
-     */
+	 * updateAmountByOrderedProductName metodu, eşleşen orderedProductName'lerin Amount'larını günceller.
+	 * 
+	 * @param
+	 */
 	@Override
 	public String updateAmountByOrderedProductName(String pName, double amount) throws Exception {
 		String message = "";
@@ -142,9 +149,10 @@ public class OrderBean extends ESGenericBean<BaseEntity> implements OrderService
 	}
 
 	/**
-     * getByOrderDate metodu, Girilen tarihten daha büyük olan tarihtki kayıtları listeler.
-     * @return records of JWS1_ORDER Tables
-     */
+	 * getByOrderDate metodu, Girilen tarihten daha büyük olan tarihtki kayıtları listeler.
+	 * 
+	 * @return records of JWS1_ORDER Tables
+	 */
 	@Override
 	public List<Jws1Order> getByOrderDate(Date date) throws Exception {
 		DateUtil dt = new DateUtil();
@@ -153,9 +161,10 @@ public class OrderBean extends ESGenericBean<BaseEntity> implements OrderService
 	}
 
 	/**
-     * getByAmountByOrderArrivalDate metodu, İki orderArrivalDate arasındaki ve Amount'ları girilen değerden büyük olanları listeler.
-     * @return records of JWS1_ORDER Tables
-     */
+	 * getByAmountByOrderArrivalDate metodu, İki orderArrivalDate arasındaki ve Amount'ları girilen değerden büyük olanları listeler.
+	 * 
+	 * @return records of JWS1_ORDER Tables
+	 */
 	@Override
 	public List<Jws1Order> getByAmountByOrderArrivalDate(double amount, Timestamp startDate, Timestamp endDate) throws Exception {
 		BigDecimal b = new BigDecimal(amount);
@@ -164,10 +173,10 @@ public class OrderBean extends ESGenericBean<BaseEntity> implements OrderService
 	}
 
 	/**
-     * getByOrderDate metodu, girilen orderedProductName ve girilen Amounttan küçük olanları listeler.
-     * orderedProductName alanı için LIKE kullanılmıştır.
-     * @return records of JWS1_ORDER Tables
-     */
+	 * getByOrderDate metodu, girilen orderedProductName ve girilen Amounttan küçük olanları listeler. orderedProductName alanı için LIKE kullanılmıştır.
+	 * 
+	 * @return records of JWS1_ORDER Tables
+	 */
 	@Override
 	public List<Jws1Order> getByOrderedProductNameByAmount(String productName, double amount) throws Exception {
 		BigDecimal b = new BigDecimal(amount);
@@ -176,9 +185,10 @@ public class OrderBean extends ESGenericBean<BaseEntity> implements OrderService
 	}
 
 	/**
-     * getByOrderDate metodu, Girilen orderedProductName'e göre gelen kayıtları listeler.
-     * @return records of JWS1_ORDER Tables
-     */
+	 * getByOrderDate metodu, Girilen orderedProductName'e göre gelen kayıtları listeler.
+	 * 
+	 * @return records of JWS1_ORDER Tables
+	 */
 	@Override
 	public List<Jws1Order> getByOrderProductName(String orderedProductName) throws Exception {
 		if(null != orderedProductName) {
@@ -189,5 +199,5 @@ public class OrderBean extends ESGenericBean<BaseEntity> implements OrderService
 		}
 	}
 
-	
+
 }
