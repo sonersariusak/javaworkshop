e6ca2114a6f665b28fc0cd3776e28dd07bdd609b
FIT\soner.sariusak <soner.sariusak@gmail.com>
2018-05-17 11:19:26 +0300

    -- orderManagement.xhtml sayfası oluşturuldu. Menüden sayfaya geçiş yapıldı.

---
+++ JWS1DYNWEB/src/com/foriba/jws1/web/page/OrderManagementSearchPage.java
@@ -0,0 +1,26 @@
+package com.foriba.jws1.web.page;
+
+
+/**
+ * 
+ * @author
+ * 
+ */
+
+public class OrderManagementSearchPage extends AbstractPage {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public OrderManagementSearchPage() throws Exception {
+		onLoad();
+	}
+
+	public String onLoad() throws Exception {
+
+		return "productName";
+
+	}
+}
